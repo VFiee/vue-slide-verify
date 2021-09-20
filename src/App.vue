d82439c4a30c974dbf253eb399e6976b914ddba3
vyron <fiee_vyron@163.com>
2021-09-20 21:01:55 +0800

    feat: init project

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import Verify from "./components/verify/index.vue"
+</script>
+
+<template>
+  <Verify />
+</template>
+
+<style>
+html,
+body {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ src/components/verify/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="verify-wrap">
+        <div class="canvas-wrap">
+            <canvas class="content"></canvas>
+            <canvas class></canvas>
+        </div>
+        <div class="refresher-wrap">
+            <img src="/@/assets/images/refresh.png" alt="刷新" />
+        </div>
+        <div class="close-wrap">
+            <img src="/@/assets/images/close.png" alt="关闭" />
+        </div>
+        <div class="slide-wrap">
+            <div class="slider"></div>
+            <div class="placeholder">向右滑动验证</div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+</script>
